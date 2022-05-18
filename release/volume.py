902dc121c503e6d921e96ab1997f9ce80a6cbc7e
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2022-05-18 21:22:37 +0200

    release 18/05/22, fix sounds and vocals, better global rendering

---
+++ release/volume.py
@@ -0,0 +1,75 @@
+from RPi import GPIO
+from time import sleep
+import alsaaudio
+
+# Change the following pins based on your application or HAT in use
+
+encoder_button = 32
+encoder_data = 36
+encoder_clk = 38
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(encoder_clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(encoder_data, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(encoder_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+m = alsaaudio.Mixer('Headphone')
+
+# Set desired minimum and maximum values
+min = 74
+max = 100
+
+# Set the volume change step size
+volume_step_size = 2
+
+is_Muted = m.getmute()[0]
+volume = m.getvolume()[0]
+
+if is_Muted == 0:
+    is_Muted = False
+else:
+    is_Muted = True
+print("Mute State: " + str(is_Muted))
+print("Volume: " + str(volume))
+print("")
+clkLastState = GPIO.input(encoder_clk)
+btnLastState = GPIO.input(encoder_button)
+
+try:
+    while True:
+        btnPushed = GPIO.input(encoder_button)
+        if (not btnLastState) and btnPushed:
+            if is_Muted:
+                is_Muted = False
+                m.setmute(0)
+                print("Mute State: " + str(is_Muted))
+                print("Volume: " + str(int(volume)))
+                print("")
+            else:
+                is_Muted = True
+                m.setmute(1)
+                print("Mute State: " + str(is_Muted))
+                print("Volume: " + str(int(volume)))
+                print("")
+            sleep(0.05)
+        else:
+            clkState = GPIO.input(encoder_clk)
+            dtState = GPIO.input(encoder_data)
+            if clkState != clkLastState:
+                if dtState != clkState:
+                    volume += volume_step_size / 2
+                    if volume > max:
+                        volume = max
+                else:
+                    volume -= volume_step_size / 2
+                    if volume < min:
+                        volume = min
+                if clkState == 1:
+                    print("Mute State: " + str(is_Muted))
+                    print("Volume: " + str(int(volume)))
+                    print("")
+                    m.setvolume(int(volume))
+            clkLastState = clkState
+        btnLastState = btnPushed
+finally:
+    GPIO.cleanup()
